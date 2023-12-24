409640276695ec7ea62d4b38df0ce81ce64cb3d1
Shubh Khoda <shubhkhoda19@gmail.com>
2023-12-24 18:59:45 +0000

    strings commit

---
+++ basics_01/strings.js
@@ -0,0 +1,22 @@
+const name = "Shubh"
+const repoCOunt = 12
+
+//console.log(name + repoCOunt);
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCOunt}`);
+// also known as string interpolation
+
+const gameName = new String("SHubhFC");
+
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('b'));
+
+const sub = gameName.substring(0,6);
+console.log(sub);
+
+
