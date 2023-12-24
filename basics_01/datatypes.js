3e249e21fdae5785c0f3f4b40bc6a27b7a153c15
Shubh Khoda <shubhkhoda19@gmail.com>
2023-12-24 18:23:26 +0000

    Stack and Heap explain

--- basics_01/datatypes.js
+++ basics_01/datatypes.js
@@ -17,6 +17,35 @@ let isLogeeinIn = true
 
 // object => 
 
-console.log(typeof null);
-console.log(undefined)
+console.log(typeof null)
 
+console.log(typeof undefined)
+
+//Primitive - 
+
+//Referenced - Arrays, Functions, object
+
+const legends = ["Leo Messi", "Luis SUarez", "Andres Iniesta", "Xavi"]
+
+ let object = {
+    name: "Shubh",
+    age : 22
+}
+
+
+let myfunc = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myfunc);
+
+// STack - Primitive 
+// Heap - Non Primitive
+
+let club = "Barcelona"
+let anotherClub = "Man City"
+
+anotherClub = "Real Madrid"
+
+console.log(club);
+console.log(anotherClub);
