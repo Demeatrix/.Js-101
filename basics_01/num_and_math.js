cca97425356c6b97ccea2c9ef7b4958f826327ac
Shubh Khoda <shubhkhoda19@gmail.com>
2024-01-03 18:07:07 +0000

    math module

--- basics_01/num_and_math.js
+++ basics_01/num_and_math.js
@@ -4,6 +4,36 @@ const balance = new Number(100)
 
 console.log(balance)
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+const floating = 3.1425468
+
+// console.log(balance.toString().length); 
+// console.log(balance.toFixed(2));
+// console.log(floating.toPrecision(4))
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+
+
+// ---------------------------++++++++++++ Maths ++++++++++--------------------------------------
+
+// console.log(Math);
+// console.log(Math.abs(-4.25));
+// console.log(Math.round(5.24685));
+// console.log(Math.min(4, 9, 8, 2));
+// console.log(Math.max(4, 8, 6, 7));
+
+
+// console.log(Math.random()); -----------------------> Gives a value between 0 & 1 only
+
+// console.log(Math.floor(Math.random() * 10) + 1); -----------------------> May give 0.9 and math.floor will convert it into 0, to solve this =1 is used
+
+// How to find out min and max
+
+const min = 10
+const max = 20
+
+console.log(Math.round(Math.random() * (max - min + 1)))
+
+
 
