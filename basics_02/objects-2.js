94f809c1ad2b911eaf0ca17225479fcfb3ef7fdb
Shubh Khoda <shubhkhoda19@gmail.com>
2024-01-07 14:42:59 +0000

    API's and JSON format

--- basics_02/objects-2.js
+++ basics_02/objects-2.js
@@ -34,3 +34,39 @@ const obj2 = {
 }
 
 const obj3 = Object.assign({}, obj1, obj2)
+
+// 
+
+const course = {
+    courseName : "js in hindi",
+    price : "0.00",
+    courseInstructor : "Shubh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor : instructor} = course  /* Object Destructuring*/ 
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// API's ---> some values that comes in form of JSON format from the server or backend
+
+// {
+//     "name" : "Shubh",
+//     "courseName" : "js in hindi",
+//     "price" : "free"
+// }
+
+
+
+// [
+//     {},
+//     {},
+//     {}
+// ]  -----> sometimes data aslo comes in the form of array of objects
+
+
+//  To understand JSON data, use tools like JSON formatter
+
+
