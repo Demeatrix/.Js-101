f022db42f099f06358e90e27ad469a515553dace
Shubh Khoda <shubhkhoda19@gmail.com>
2024-01-16 14:49:08 +0000

    for

---
+++ iterations_05/for.js
@@ -0,0 +1,23 @@
+// for
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if(element == 7) {
+//         console.log("Its a shot");
+//     }
+//     console.log(element);
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`outer loop ${i}`);
+//     for (let  j = 0; j < 10; j++) {
+//         console.log(`inside loop ${j}`);
+//     }
+// }
+
+let myArr = ['Messi', "Pedri", "Gavi", "Busquets", "Iniesta"]
+
+for (let index = 0; index < myArr.length; index++) {
+    console.log(myArr[index]);
+}
